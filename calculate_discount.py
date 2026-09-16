c3807444c961873e81cdab59486aae5c89e8d88e
ji798 <jisuazom@uamv.edu.ni>
2026-09-16 12:19:44 -0600

    Refactor discount calculation variable name

--- calculate_discount.py
+++ calculate_discount.py
@@ -1,16 +1,16 @@
 def cal_discount(subtotal, client_type):
     # Descuento según el monto
     if subtotal >= 1000:
-        desc = 0.15
+        discount = 0.15
     elif subtotal >= 500:
-        desc = 0.10
+        discount = 0.10
     else:
-        desc = 0.0
+        discount = 0.0
 
     # Descuento adicional por tipo de cliente
     if client_type.lower() == "vip":
-        desc += 0.10
+        discount += 0.10
     elif client_type.lower() == "frecuente":
-        desc += 0.05
+        discount += 0.05
 
-    return subtotal * desc
+    return subtotal * discount
